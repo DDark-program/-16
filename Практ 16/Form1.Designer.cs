e1ed96927fe633296c23b721a90d60ee8e76edcc
Данила(DDark) <(DDark)@DESKTOP-8SPOKD2>
2020-12-22 14:50:30 +0300

    Добавьте файлы проекта.

---
+++ Практ 16/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Практ_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.water = new System.Windows.Forms.PictureBox();
+            this.boiler = new System.Windows.Forms.PictureBox();
+            this.fon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.water)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boiler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Собрано капель:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 21);
+            this.label2.TabIndex = 2;
+            // 
+            // water
+            // 
+            this.water.Image = global::Практ_16.Properties.Resources.water;
+            this.water.Location = new System.Drawing.Point(357, 136);
+            this.water.Name = "water";
+            this.water.Size = new System.Drawing.Size(66, 76);
+            this.water.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.water.TabIndex = 0;
+            this.water.TabStop = false;
+            // 
+            // boiler
+            // 
+            this.boiler.BackColor = System.Drawing.SystemColors.Window;
+            this.boiler.Image = global::Практ_16.Properties.Resources.boiler1;
+            this.boiler.Location = new System.Drawing.Point(409, 367);
+            this.boiler.Name = "boiler";
+            this.boiler.Size = new System.Drawing.Size(194, 189);
+            this.boiler.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boiler.TabIndex = 0;
+            this.boiler.TabStop = false;
+            // 
+            // fon
+            // 
+            this.fon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fon.Image = global::Практ_16.Properties.Resources.nicolas_appenzeller_cmc_clubhouse_by_tardifice_d97h0uq;
+            this.fon.Location = new System.Drawing.Point(0, 0);
+            this.fon.Name = "fon";
+            this.fon.Size = new System.Drawing.Size(1000, 568);
+            this.fon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fon.TabIndex = 0;
+            this.fon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 568);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.water);
+            this.Controls.Add(this.boiler);
+            this.Controls.Add(this.fon);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Игра \"собери воду\"";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp_1);
+            ((System.ComponentModel.ISupportInitialize)(this.water)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boiler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox fon;
+        private System.Windows.Forms.PictureBox boiler;
+        private System.Windows.Forms.PictureBox water;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Практ 16/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практ_16
+{
+    public partial class Form1 : Form
+    {
+        int vector, score=0;
+        Rectangle r1, r2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Игра <Собери капли>";
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            boiler.Parent = fon;
+            boiler.BackColor = Color.Transparent;
+            water.Parent = fon;
+            water.BackColor = Color.Transparent;
+            Random r = new Random();
+            water.Left = r.Next(100, 700);
+            water.Top = -150;
+            score = 1;
+            vector = 0;
+        }
+
+        private void Form1_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            vector = 0;
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                vector = -3;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                vector = 3;
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            int fall_speed = 2;
+            water.Top = water.Top + fall_speed;
+            boiler.Left = boiler.Left + vector;
+            if (water.Top >= fon.Height)
+            {
+                timer1.Enabled = false;
+                DialogResult result = MessageBox.Show("Вы проиграли.Начать заново? ", "Проигрыш",MessageBoxButtons.OKCancel);
+                if (result==DialogResult.OK) 
+                {
+                    timer1.Enabled = true;
+                    Random r = new Random();
+                    water.Top = -150;
+                    water.Left = r.Next(100, 700);
+                }
+                else this.Close();
+            }
+            r1 = water.DisplayRectangle;r2 = boiler.DisplayRectangle;
+            r1.Location = water.Location;r2.Location = boiler.Location;
+            if (r1.IntersectsWith(r2))
+            {
+                Random r = new Random();
+                water.Top = -150;
+                water.Left = r.Next(100, 700);
+                score++;
+                label1.Text = score.ToString();
+            }
+            if (boiler.Location.X < 0)
+            {
+                boiler.Location = new Point(900,boiler.Location.Y);
+            }
+            if (boiler.Location.X > 900)
+            {
+                boiler.Location = new Point(0, boiler.Location.Y);
+            }
+        }
+    }
+}
